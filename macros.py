b24ea264a957212180046f358520a0110a394b8c
yuossfalaa <yuossfalla@gmail.com>
2024-02-18 13:01:19 +0200

    adding micros

---
+++ macros.py
@@ -0,0 +1,44 @@
+NUM_LAYERS = 12
+NUM_HEAD = 16
+N_DIM = 1024
+PREFIX_MODE = 1
+NUM_QUANTIZERS = 8
+SAMPLE_RATE = 24000
+
+lang2token = {
+    'zh': "[ZH]",
+    'ja': "[JA]",
+    "en": "[EN]",
+    "ar": "[AR]",
+    'mix': "",
+}
+
+lang2code = {
+    'zh': 0,
+    'ja': 1,
+    "en": 2,
+    "ar": 3,
+}
+
+token2lang = {
+    '[ZH]': "zh",
+    '[JA]': "ja",
+    "[EN]": "en",
+    "[AR]": "ar",
+    "": "mix"
+}
+
+code2lang = {
+    0: 'zh',
+    1: 'ja',
+    2: "en",
+    3: "ar",
+}
+
+langdropdown2token = {
+    'English': "[EN]",
+    '中文': "[ZH]",
+    '日本語': "[JA]",
+    'عربى': "[AR]",
+    'Mix': "",
+}
