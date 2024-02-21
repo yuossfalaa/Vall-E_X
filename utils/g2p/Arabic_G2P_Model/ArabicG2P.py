95740b43bac30c5f49b3dd70b5b16cade01872ef
yuossfalaa <yuossfalla@gmail.com>
2024-02-21 17:00:32 +0200

    adding Search space

--- utils/g2p/Arabic_G2P_Model/ArabicG2P.py
+++ utils/g2p/Arabic_G2P_Model/ArabicG2P.py
@@ -1,14 +1,30 @@
-from pathlib import Path
+import json
+
 from dp.phonemizer import Phonemizer
+
+SEARCH_SPACE_PATH = "./utils/g2p/Arabic_G2P_Model/Data/DataSet.json"
+
+
+def load_data(json_path):
+    with open(json_path, "r", encoding="utf-8") as file:  # Open the file in read mode ,with utf-8 for arabic
+        try:
+            json_data = json.load(file)  # Load the JSON data from the file
+            return json_data
+        except json.JSONDecodeError as e:
+            print(f"Error loading JSON: {e}")  # Handle potential JSON decoding errors
 
 
 class ArabicG2P:
     def __init__(self):
-
-        script_dir = Path(__file__).resolve().parent
-        rel_path = "checkpoints/best_model.pt"
-        abs_file_path = script_dir / rel_path
-        self.phonemizer = Phonemizer.from_checkpoint(abs_file_path)
+        self.phonemizer = Phonemizer.from_checkpoint("./utils/g2p/Arabic_G2P_Model/checkpoints/best_model.pt")
+        self.SearchSpace = load_data(SEARCH_SPACE_PATH)
 
     def G2P(self, sentence: str) -> str:
-        return self.phonemizer(sentence, lang='ar')
+        words = sentence.split()
+        Phoneme_results = []
+        for word in words:
+            if word in self.SearchSpace:
+                Phoneme_results.append(self.SearchSpace[word])
+            else:
+                Phoneme_results.append(self.phonemizer(word, lang='ar'))
+        return ' '.join(Phoneme_results)
