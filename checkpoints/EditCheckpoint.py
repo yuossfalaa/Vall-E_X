fca96b284e4e360419b3f077275c6e33bf3fe74f
yuossfalaa <yuossfalla@gmail.com>
2024-04-16 21:57:22 +0200

    Adding the ability to edit a checkpoint and adda 4th language embedding

---
+++ checkpoints/EditCheckpoint.py
@@ -0,0 +1,13 @@
+# This file adds the ability to embed a 4th language to the pretrained model
+
+import torch
+
+device = torch.device("cpu")
+checkpoint = torch.load('checkpoints/vallex-checkpoint.pt')
+
+checkpoint['model']['ar_language_embedding.word_embeddings.weight'] = torch.cat(
+    [checkpoint['model']['ar_language_embedding.word_embeddings.weight'].to(device), torch.rand(1, 1024).to(device)])
+checkpoint['model']['nar_language_embedding.word_embeddings.weight'] = torch.cat(
+    [checkpoint['model']['nar_language_embedding.word_embeddings.weight'].to(device), torch.rand(1, 1024).to(device)])
+
+torch.save(checkpoint, 'vallex-checkpoint.pt')
